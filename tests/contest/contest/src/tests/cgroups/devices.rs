dc5a1c16b0bb7db36df363c2c8a31fce2dca1bc5
sat0ken <15720506+sat0ken@users.noreply.github.com>
2025-01-18 22:04:55 +0900

    read cgroup devices.list and check spec file
    Signed-off-by: sat0ken <15720506+sat0ken@users.noreply.github.com>

--- tests/contest/contest/src/tests/cgroups/devices.rs
+++ tests/contest/contest/src/tests/cgroups/devices.rs
@@ -1,10 +1,13 @@
-use std::path::Path;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::path::{Path, PathBuf};
 
-use anyhow::Context;
+use anyhow::{bail, Context, Result};
 use oci_spec::runtime::{
     LinuxBuilder, LinuxDeviceCgroup, LinuxDeviceCgroupBuilder, LinuxDeviceType,
     LinuxResourcesBuilder, Spec, SpecBuilder,
 };
+use contest::utils::test_utils::CGROUP_ROOT;
 use test_framework::{test_result, ConditionalTest, TestGroup, TestResult};
 
 use crate::utils::test_outside_container;
@@ -30,7 +33,7 @@ fn linux_device_build(
         .unwrap()
 }
 
-fn create_spec(cgroup_name: &str, devices: Vec<LinuxDeviceCgroup>) -> anyhow::Result<Spec> {
+fn create_spec(cgroup_name: &str, devices: Vec<LinuxDeviceCgroup>) -> Result<Spec> {
     let spec = SpecBuilder::default()
         .linux(
             LinuxBuilder::default()
@@ -50,6 +53,85 @@ fn create_spec(cgroup_name: &str, devices: Vec<LinuxDeviceCgroup>) -> anyhow::Re
     Ok(spec)
 }
 
+fn get_allow_linux_devices(path: &Path) -> Result<Vec<LinuxDeviceCgroup>> {
+    let file = File::open(path).unwrap()?;
+    let reader = BufReader::new(file);
+    let mut devices :Vec<LinuxDeviceCgroup> = vec![];
+
+    for line in reader.lines() {
+        let line = line?;
+        let parts: Vec<&str> = line.split_whitespace().collect();
+        if parts.len() == 3 {
+            let device_type = match parts[0] {
+                "b" => LinuxDeviceType::B,
+                "c" => LinuxDeviceType::C,
+                "*" => LinuxDeviceType::A,
+                _ => continue,
+            };
+            // read major, minor number
+            let major_minor: Vec<&str> = parts[1].split(':').collect();
+            if major_minor.len() != 2 {
+                // ignore invalid format
+                continue;
+            }
+            let major = if major_minor[0] == "*" {
+                None
+            } else {
+                major_minor[0].parse::<i64>().ok()
+            };
+            let minor = if major_minor[1] == "*" {
+                None
+            } else {
+                major_minor[1].parse::<i64>().ok()
+            };
+            // read access string
+            let access = parts[2].to_string();
+            devices.push(linux_device_build(
+                device_type, major, minor, access,
+            ))
+        }
+    }
+
+    Ok(devices);
+}
+
+fn validate_linux_devices(cgroup_name: &str, spec: &Spec) -> Result<()> {
+    let cgroup_path = PathBuf::from(CGROUP_ROOT)
+        .join("devices")
+        .join("runtime-test")
+        .join(cgroup_name)
+        .join("devices.list");
+    let linux_devices = get_allow_linux_devices(&cgroup_path)?;
+
+    let resources = spec.linux().as_ref().unwrap().resources().as_ref().unwrap();
+    let spec_linux_devices = resources.devices().as_ref().unwrap();
+
+    for spec_linux_device in spec_linux_devices {
+        if spec_linux_device.allow() {
+            let mut found = false;
+            for linux_device in linux_devices.clone() {
+                if linux_device.typ() == spec_linux_device.typ() &&
+                    linux_device.major() == spec_linux_device.major() &&
+                    linux_device.minor() == spec_linux_device.minor() &&
+                    linux_device.access() == spec_linux_device.access() {
+                        found = true;
+                }
+            }
+            if !found {
+                bail!(
+                    "allow linux device {}:{}:{}:{} not found, exists in spec",
+                    spec_linux_device.typ(),
+                    spec_linux_device.major(),
+                    spec_linux_device.minor(),
+                    spec_linux_device.access()
+                );
+            }
+        }
+    }
+
+    Ok(())
+}
+
 fn test_devices_cgroups() -> TestResult {
     let cgroup_name = "test_devices_cgroups";
     let linux_devices = vec![
@@ -59,14 +141,11 @@ fn test_devices_cgroups() -> TestResult {
     ];
     let spec = test_result!(create_spec(cgroup_name, linux_devices));
 
-    let test_result = test_outside_container(spec, &|data| {
+    test_outside_container(spec.clone(), &|data| {
         test_result!(check_container_created(&data));
+        test_result!(validate_linux_devices(&cgroup_name, &spec));
         TestResult::Passed
-    });
-    if let TestResult::Failed(_) = test_result {
-        return test_result;
-    }
-    test_result
+    })
 }
 
 pub fn get_test_group() -> TestGroup {
@@ -74,7 +153,7 @@ pub fn get_test_group() -> TestGroup {
     let linux_cgroups_devices = ConditionalTest::new(
         "test_linux_cgroups_devices",
         Box::new(can_run),
-        Box::new(crate::tests::cgroups::devices::test_devices_cgroups),
+        Box::new(test_devices_cgroups),
     );
 
     test_group.add(vec![Box::new(linux_cgroups_devices)]);
