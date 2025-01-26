9f005f38f816c258fb086869fa6d4754ddb29e0c
utam0k <k0ma@utam0k.jp>
2025-01-26 20:53:08 +0900

    selinux: Refactoring
    Signed-off-by: utam0k <k0ma@utam0k.jp>

--- experiment/selinux/src/lib.rs
+++ experiment/selinux/src/lib.rs
@@ -1,5 +1,10 @@
+pub mod error;
+pub mod label;
+pub mod mode;
 pub mod selinux;
-pub mod selinux_label;
+pub mod setting;
 pub mod tools;
 
+pub use error::*;
+pub use mode::SELinuxMode;
 pub use selinux::SELinux;
