79b395c883f895d1ca3759b7b7b59e9b2dcf5453
moz-sec <m0253c@gmail.com>
2025-06-05 21:57:05 +0900

    feat: randomize the numbers to be mapped
    Signed-off-by: moz-sec <m0253c@gmail.com>

--- tests/contest/contest/src/tests/uid_mappings/uid_mappings_test.rs
+++ tests/contest/contest/src/tests/uid_mappings/uid_mappings_test.rs
@@ -4,10 +4,30 @@ use oci_spec::runtime::{
     LinuxBuilder, LinuxIdMapping, LinuxIdMappingBuilder, LinuxNamespace, LinuxNamespaceBuilder,
     LinuxNamespaceType, ProcessBuilder, Spec, SpecBuilder,
 };
+use rand::Rng;
 use test_framework::{Test, TestGroup, TestResult};
 
 use crate::utils::test_inside_container;
 use crate::utils::test_utils::CreateOptions;
+
+// The `host_id` is randomly chosen between 1000 and 2000, while the `size`
+// is randomly chosen between 100 and 2500. The `container_id` is fixed at 0.
+fn generate_random_id_mappings() -> Vec<LinuxIdMapping> {
+    let mut rng = rand::rng();
+
+    let host_id: u32 = rng.random_range(1000..=2000);
+    let size: u32 = rng.random_range(100..=2500);
+
+    // container_id 0 must exist, otherwise container creation will fail
+    let id_mapping = LinuxIdMappingBuilder::default()
+        .host_id(host_id)
+        .container_id(0_u32)
+        .size(size)
+        .build()
+        .expect("error in building LinuxIdMapping");
+
+    vec![id_mapping]
+}
 
 fn create_spec(uid_mappings: Vec<LinuxIdMapping>, gid_mappings: Vec<LinuxIdMapping>) -> Spec {
     let mut namespaces: Vec<LinuxNamespace> = oci_spec::runtime::get_default_namespaces();
@@ -37,19 +57,8 @@ fn create_spec(uid_mappings: Vec<LinuxIdMapping>, gid_mappings: Vec<LinuxIdMappi
 }
 
 fn uid_mappings_test() -> TestResult {
-    let uid_mappings = vec![LinuxIdMappingBuilder::default()
-        .host_id(1000_u32)
-        .container_id(0_u32)
-        .size(2000_u32)
-        .build()
-        .unwrap()];
-
-    let gid_mappings = vec![LinuxIdMappingBuilder::default()
-        .host_id(1000_u32)
-        .container_id(0_u32)
-        .size(3000_u32)
-        .build()
-        .unwrap()];
+    let uid_mappings = generate_random_id_mappings();
+    let gid_mappings = generate_random_id_mappings();
 
     let spec = create_spec(uid_mappings, gid_mappings);
     test_inside_container(&spec, &CreateOptions::default(), &|_| Ok(()))
