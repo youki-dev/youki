56b9f61a45d064712978073e3dc8d1c6c8e2539d
nayuta-ai <nayuta723@gmail.com>
2025-06-05 16:52:17 +0000

    delete tokio from tests
    Signed-off-by: nayuta-ai <nayuta723@gmail.com>

--- tests/contest/runtimetest/src/tests.rs
+++ tests/contest/runtimetest/src/tests.rs
@@ -7,7 +7,11 @@ use std::os::unix::fs::{FileTypeExt, PermissionsExt};
 use std::path::Path;
 
 use anyhow::{bail, Result};
-use futures::stream::TryStreamExt;
+use netlink_packet_core::{NetlinkMessage, NetlinkPayload, NLM_F_REQUEST};
+use netlink_packet_route::link::{LinkAttribute, LinkMessage};
+use netlink_packet_route::RouteNetlinkMessage;
+use netlink_sys::protocols::NETLINK_ROUTE;
+use netlink_sys::Socket;
 use nix::errno::Errno;
 use nix::libc;
 use nix::mount::{mount, MsFlags};
@@ -20,9 +24,7 @@ use oci_spec::runtime::{
     LinuxDevice, LinuxDeviceType, LinuxIdMapping, LinuxSchedulerPolicy, PosixRlimit,
     PosixRlimitType, Spec,
 };
-use rtnetlink::new_connection;
 use tempfile::Builder;
-use tokio::runtime::Runtime;
 
 use crate::utils::{
     self, test_dir_read_access, test_dir_write_access, test_read_access, test_write_access,
@@ -1010,6 +1012,8 @@ pub fn validate_uid_mappings(spec: &Spec) {
 }
 
 pub fn validate_net_devices(spec: &Spec) {
+    let mut socket = Socket::new(NETLINK_ROUTE).unwrap();
+    socket.bind_auto().unwrap();
     let linux = spec.linux().as_ref().unwrap();
     if let Some(net_devices) = linux.net_devices() {
         for (name, net_device) in net_devices {
@@ -1018,27 +1022,33 @@ pub fn validate_net_devices(spec: &Spec) {
                 .as_ref()
                 .filter(|d| !d.is_empty())
                 .map_or(name.clone(), |d| d.to_string());
-            validate_net_device(net_device_name);
+
+            let mut message = LinkMessage::default();
+            message
+                .attributes
+                .push(LinkAttribute::IfName(net_device_name.clone()));
+
+            let mut req = NetlinkMessage::from(RouteNetlinkMessage::GetLink(message));
+            req.header.flags = NLM_F_REQUEST;
+            req.finalize();
+
+            let mut send_buf = vec![0; req.header.length as usize];
+            req.serialize(&mut send_buf[..]);
+            socket.send(&send_buf[..], 0).unwrap();
+
+            let mut receive_buf = vec![0u8; 4096];
+            let n_received = socket.recv(&mut &mut receive_buf[..], 0).unwrap();
+            let bytes = &receive_buf[..n_received];
+            let rx_packet = <NetlinkMessage<RouteNetlinkMessage>>::deserialize(bytes).unwrap();
+
+            match rx_packet.payload {
+                NetlinkPayload::InnerMessage(RouteNetlinkMessage::NewLink(_link)) => {
+                    println!("network device {} is present", net_device_name);
+                }
+                _ => {
+                    eprintln!("network device {} is not present", net_device_name);
+                }
+            }
         }
     }
-}
-
-fn validate_net_device(net_device_name: String) {
-    let rt = Runtime::new().unwrap();
-    rt.block_on(async {
-        let (connection, handle, _) = new_connection().unwrap();
-        tokio::spawn(connection);
-
-        let mut links = handle
-            .link()
-            .get()
-            .match_name(net_device_name.clone())
-            .execute();
-
-        if links.try_next().await.unwrap().is_some() {
-            println!("network device {} is present", net_device_name);
-        } else {
-            eprintln!("network device {} is not present", net_device_name);
-        }
-    });
 }
