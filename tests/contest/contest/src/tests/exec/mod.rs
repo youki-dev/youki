bdab39b5ac37a1709ad604d861df98be42932cbb
tommady <tommady@users.noreply.github.com>
2025-08-20 14:32:26 +0000

    fix preserve_fds_test flank failure
    Signed-off-by: tommady <tommady@users.noreply.github.com>

--- tests/contest/contest/src/tests/exec/mod.rs
+++ tests/contest/contest/src/tests/exec/mod.rs
@@ -1,6 +1,25 @@
+mod ignore_paused_test;
 mod preserve_fds_test;
 
+use anyhow::{Context, Result};
+use oci_spec::runtime::{ProcessBuilder, Spec, SpecBuilder};
 use test_framework::{Test, TestGroup};
+
+fn create_spec() -> Result<Spec> {
+    SpecBuilder::default()
+        .process(
+            ProcessBuilder::default()
+                .args(
+                    ["sleep", "1000"]
+                        .iter()
+                        .map(|s| s.to_string())
+                        .collect::<Vec<String>>(),
+                )
+                .build()?,
+        )
+        .build()
+        .context("failed to create spec")
+}
 
 pub fn get_exec_test() -> TestGroup {
     let mut test_group = TestGroup::new("exec");
@@ -10,7 +29,15 @@ pub fn get_exec_test() -> TestGroup {
         Box::new(preserve_fds_test::preserve_fds_test),
     );
 
-    test_group.add(vec![Box::new(preserve_fds_test)]);
+    let ignore_paused_test = Test::new(
+        "ignore_paused_test",
+        Box::new(ignore_paused_test::ignore_paused_test),
+    );
+
+    test_group.add(vec![
+        Box::new(preserve_fds_test),
+        Box::new(ignore_paused_test),
+    ]);
 
     test_group
 }
--- tests/contest/contest/src/tests/exec/preserve_fds_test.rs
+++ tests/contest/contest/src/tests/exec/preserve_fds_test.rs
@@ -1,31 +1,17 @@
+use std::fs;
 use std::os::fd::AsRawFd;
 
-use anyhow::{anyhow, Context, Result};
-use oci_spec::runtime::{ProcessBuilder, Spec, SpecBuilder};
+use anyhow::anyhow;
+use nix::fcntl::{fcntl, FcntlArg, FdFlag};
+use nix::unistd::dup2;
+use test_framework::{test_result, TestResult};
 
 use crate::utils::test_utils::{
     check_container_created, exec_container, start_container, test_outside_container,
 };
-use test_framework::{test_result, TestResult};
-
-fn create_spec() -> Result<Spec> {
-    SpecBuilder::default()
-        .process(
-            ProcessBuilder::default()
-                .args(
-                    ["sleep", "1000"]
-                        .iter()
-                        .map(|s| s.to_string())
-                        .collect::<Vec<String>>(),
-                )
-                .build()?,
-        )
-        .build()
-        .context("failed to create spec")
-}
 
 pub(crate) fn preserve_fds_test() -> TestResult {
-    let spec = test_result!(create_spec());
+    let spec = test_result!(super::create_spec());
 
     test_outside_container(&spec, &|data| {
         test_result!(check_container_created(&data));
@@ -38,19 +24,21 @@ pub(crate) fn preserve_fds_test() -> TestResult {
             return TestResult::Failed(anyhow!("container start failed"));
         }
 
-        use nix::unistd::dup2;
-        use std::fs;
         fs::write(dir.join("preserve-fds.test"), b"hello world\n")
             .expect("write preserve-fds.test failed");
+
         let file =
             fs::File::open(dir.join("preserve-fds.test")).expect("open preserve-fds.test failed");
         let fd = file.as_raw_fd();
-        dup2(fd, 4).expect("dup2(fd, 4) failed");
+
+        let flags = FdFlag::from_bits_truncate(fcntl(fd, FcntlArg::F_GETFD).expect(""));
+        fcntl(fd, FcntlArg::F_SETFD(flags & !FdFlag::FD_CLOEXEC)).expect("");
+        dup2(fd, 3).expect("dup2 failed");
 
         let (stdout, _) = exec_container(
             id,
             dir,
-            &["--preserve-fds=2", "cat", "/proc/self/fd/4"],
+            &["--preserve-fds=1", "cat", "/proc/self/fd/3"],
             None,
         )
         .expect("exec failed");
@@ -58,6 +46,8 @@ pub(crate) fn preserve_fds_test() -> TestResult {
         if !stdout.contains("hello world") {
             return TestResult::Failed(anyhow!("unexpected output: {}", stdout));
         }
+
+        drop(file);
 
         TestResult::Passed
     })
