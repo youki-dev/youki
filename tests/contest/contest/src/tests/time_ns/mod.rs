72e02b4764e420b95f04e4993c61d93402e703b0
Carson Weeks <mail@carsonweeks.com>
2025-10-19 00:12:15 -0400

    user + time namespace test
    Signed-off-by: Carson Weeks <mail@carsonweeks.com>

--- tests/contest/contest/src/tests/time_ns/mod.rs
+++ tests/contest/contest/src/tests/time_ns/mod.rs
@@ -1,12 +1,12 @@
-use std::collections::HashMap;
-
 use anyhow::{Context, Ok, Result};
 use oci_spec::runtime::{
-    LinuxBuilder, LinuxNamespace, LinuxNamespaceType, LinuxTimeOffset, ProcessBuilder, Spec,
+    LinuxBuilder, LinuxIdMappingBuilder, LinuxNamespace, LinuxNamespaceType, ProcessBuilder, Spec,
     SpecBuilder,
 };
 use test_framework::{Test, TestGroup, TestResult, test_result};
 
+mod util;
+use crate::tests::time_ns::util::create_time_offset;
 use crate::utils::test_inside_container;
 use crate::utils::test_utils::CreateOptions;
 
@@ -96,31 +96,51 @@ fn create_spec_for_timens_no_offsets() -> Result<Spec> {
         .context("failed to create spec")
 }
 
-fn create_time_offset(
-    boottime_secs: i64,
-    boottime_nanosecs: u32,
-    monotonic_secs: i64,
-    monotonic_nanosecs: u32,
-) -> HashMap<String, LinuxTimeOffset> {
-    let time_offsets = [
-        (
-            "boottime".to_owned(),
-            LinuxTimeOffset::default()
-                .set_secs(Some(boottime_secs))
-                .set_nanosecs(Some(boottime_nanosecs))
-                .to_owned(),
-        ),
-        (
-            "monotonic".to_owned(),
-            LinuxTimeOffset::default()
-                .set_secs(Some(monotonic_secs))
-                .set_nanosecs(Some(monotonic_nanosecs))
-                .to_owned(),
-        ),
-    ]
-    .into_iter()
-    .collect::<HashMap<_, _>>();
-    time_offsets
+fn create_spec_for_set_times_plus_userns() -> Result<Spec> {
+    let mut default_namespaces: Vec<LinuxNamespace> = oci_spec::runtime::get_default_namespaces();
+    default_namespaces.push(
+        LinuxNamespace::default()
+            .set_typ(LinuxNamespaceType::Time)
+            .to_owned(),
+    );
+
+    let id_mapping = LinuxIdMappingBuilder::default()
+        .host_id(100000_u32)
+        .container_id(0_u32)
+        .size(65534_u32)
+        .build()
+        .expect("error in building LinuxIdMapping");
+
+    let gid_mapping = LinuxIdMappingBuilder::default()
+        .host_id(200000_u32)
+        .container_id(0_u32)
+        .size(65534_u32)
+        .build()
+        .expect("error in building LinuxIdMapping");
+
+    let time_offsets = create_time_offset(1337, 3141519, 7881, 2718281);
+
+    SpecBuilder::default()
+        .process(
+            ProcessBuilder::default()
+                .args(
+                    ["runtimetest", "time_offsets"]
+                        .iter()
+                        .map(|s| s.to_string())
+                        .collect::<Vec<String>>(),
+                )
+                .build()?,
+        )
+        .linux(
+            LinuxBuilder::default()
+                .time_offsets(time_offsets)
+                .namespaces(default_namespaces)
+                .uid_mappings(vec![id_mapping])
+                .gid_mappings(vec![gid_mapping])
+                .build()?,
+        )
+        .build()
+        .context("failed to create spec")
 }
 
 fn set_times_test() -> TestResult {
@@ -138,6 +158,11 @@ fn timens_no_offsets_test() -> TestResult {
     test_inside_container(&spec, &CreateOptions::default(), &|_| Ok(()))
 }
 
+fn timens_and_userns_test() -> TestResult {
+    let spec = test_result!(create_spec_for_set_times_plus_userns());
+    test_inside_container(&spec, &CreateOptions::default(), &|_| Ok(()))
+}
+
 pub fn get_time_ns_test() -> TestGroup {
     let mut test_group = TestGroup::new("time_ns");
     let test1 = Test::new("simple timens", Box::new(set_times_test));
@@ -146,7 +171,13 @@ pub fn get_time_ns_test() -> TestGroup {
         Box::new(with_no_time_ns_but_time_offsets_test),
     );
     let test3 = Test::new("timens with no offsets", Box::new(timens_no_offsets_test));
-    test_group.add(vec![Box::new(test1), Box::new(test2), Box::new(test3)]);
+    let test4 = Test::new("simple timens + userns", Box::new(timens_and_userns_test));
+    test_group.add(vec![
+        Box::new(test1),
+        Box::new(test2),
+        Box::new(test3),
+        Box::new(test4),
+    ]);
 
     test_group
 }
