cc539824167d917bf264b91a62f7e949aa1c2fd5
utam0k <utam0k@users.noreply.github.com>
2025-12-06 06:39:59 +0000

    deploy: f31e1fe789d019e6dc9dc03b9df277f0ca787d13

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-4415311c.js',
+                path_to_root + 'searchindex-d24fbc1d.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
