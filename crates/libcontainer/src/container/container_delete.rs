c835fef968df97c4192b3aa706ac675cc961797a
Irving Mondragón <IrvingMg@users.noreply.github.com>
2026-01-26 11:56:13 +0100

    Pass State directly to `run_hooks` instead of Container reference (#3360)
    * Pass State directly to run_hooks instead of Container reference
    
    Signed-off-by: Irving Mondragón <mirvingr@gmail.com>
    
    * Refactor run_hooks to borrow State instead of cloning
    
    Signed-off-by: Irving Mondragón <mirvingr@gmail.com>
    
    ---------
    
    Signed-off-by: Irving Mondragón <mirvingr@gmail.com>

--- crates/libcontainer/src/container/container_delete.rs
+++ crates/libcontainer/src/container/container_delete.rs
@@ -98,7 +98,7 @@ impl Container {
                     })?;
 
                     if let Some(hooks) = config.hooks.as_ref() {
-                        hooks::run_hooks(hooks.poststop().as_ref(), Some(self), None, None)
+                        hooks::run_hooks(hooks.poststop().as_ref(), Some(&self.state), None, None)
                             .map_err(|err| {
                                 tracing::error!(err = ?err, "failed to run post stop hooks");
                                 err
