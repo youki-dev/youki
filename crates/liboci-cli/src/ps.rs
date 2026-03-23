916991ef63a83a858192923347e1699b8294bac4
0xchris <56232580+xvchris@users.noreply.github.com>
2026-03-23 20:23:15 +0800

    Merge pull request #3456 from xvchris/docs/add-help-descriptions
    docs(liboci-cli): add missing help descriptions to arguments and options

--- crates/liboci-cli/src/ps.rs
+++ crates/liboci-cli/src/ps.rs
@@ -6,6 +6,7 @@ pub struct Ps {
     /// format to display processes: table or json (default: "table")
     #[clap(short, long, default_value = "table")]
     pub format: String,
+    /// Container identifier
     #[clap(value_parser = clap::builder::NonEmptyStringValueParser::new(), required = true)]
     pub container_id: String,
     /// options will be passed to the ps utility
