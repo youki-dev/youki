4b033bfd35b6b0713e4650d9c3fc0abf1d3b7c1f
Yashodhan Joshi <yjdoc2@gmail.com>
2026-06-10 17:33:07 +0530

    fix: handle error properly for vergen update
    Signed-off-by: Yashodhan Joshi <yjdoc2@gmail.com>

--- crates/youki/build.rs
+++ crates/youki/build.rs
@@ -2,17 +2,24 @@ use anyhow::Result;
 use vergen_gitcl::{Emitter, Gitcl, Rustc};
 
 pub fn main() -> Result<()> {
-    if Emitter::default()
-        .add_instructions(&Gitcl::all_git())?
+    // git failing is possible as we might be building in a bare
+    // repo without .git history ; which is why we have a workaround
+    // rustc failure is rarer, and we do not have a workaround in place
+    // so we only match for error of git, and bubble up all other errors
+    // via ? do error out the build
+    // also see https://github.com/rustyhorde/vergen/issues/174#issuecomment-3631861105
+    match Emitter::default()
         .add_instructions(&Rustc::all_rustc())?
-        .emit()
-        .is_err()
+        .add_instructions(&Gitcl::all_git())
     {
-        // currently we only inject git sha, so just this
-        // else we will need to think of more elegant way to check
-        // what failed, and what needs to be added
-        println!("cargo:rustc-env=VERGEN_GIT_SHA=unknown");
-        println!("cargo:rustc-env=VERGEN_RUSTC_SEMVER=unknown");
+        Ok(emitter) => emitter.emit()?,
+        Err(_) => {
+            // currently we only inject git sha, so just this
+            // else we will need to think of more elegant way to check
+            // what failed, and what needs to be added
+            println!("cargo:rustc-env=VERGEN_GIT_SHA=unknown");
+            println!("cargo:rustc-env=VERGEN_RUSTC_SEMVER=unknown");
+        }
     }
 
     // Embed libseccomp version at build time (only when seccomp feature is enabled)
