5da6cc96f0eb9e9a9c1aca37258aafc36f94e79b
UiJong (Jace), Yang <sullung2yo@gmail.com>
2026-06-20 05:59:17 +0900

    test: Implement libcontainer config methods failure test cases (#3602)
    Signed-off-by: Scanf-s <sullung2yo@gmail.com>

--- crates/libcontainer/src/config.rs
+++ crates/libcontainer/src/config.rs
@@ -132,4 +132,26 @@ mod tests {
         assert_eq!(act, config);
         Ok(())
     }
+
+    #[test]
+    fn test_config_save_failed_to_create_config_file() -> Result<()> {
+        let container_id = "sample";
+        let spec = Spec::default();
+        let tmp = tempfile::tempdir().expect("create temp dir");
+        let config = YoukiConfig::from_spec(&spec, container_id)?;
+
+        let invalid_dir = tmp.path().join("invalid");
+        let result = config.save(&invalid_dir);
+        assert!(matches!(result, Err(ConfigError::SaveIO { .. })));
+        Ok(())
+    }
+
+    #[test]
+    fn test_config_load_failed_to_open() -> Result<()> {
+        let tmp = tempfile::tempdir().expect("create temp dir");
+        let invalid_dir = tmp.path().join("invalid");
+        let result = YoukiConfig::load(&invalid_dir);
+        assert!(matches!(result, Err(ConfigError::LoadIO { .. })));
+        Ok(())
+    }
 }
