2f69730e6a657d2c3acae72c24f3d703d8b7c61e
Yusuke Sakurai <yusuke.sakurai@3-shake.com>
2026-06-25 21:55:37 +0000

    libkrun rust
    Signed-off-by: Yusuke Sakurai <yusuke.sakurai@3-shake.com>

--- crates/libcontainer/src/container/init_builder.rs
+++ crates/libcontainer/src/container/init_builder.rs
@@ -79,9 +79,9 @@ impl InitContainerBuilder {
         // (mirrors runc's checkTerminal called at the top of runner.run())
         self.base.check_terminal(&spec, self.detached)?;
 
-        let container_dir = self.create_container_dir()?;
-
         let spec = self.base.executor.modify_spec(spec)?;
+
+        let container_dir = self.create_container_dir()?;
 
         let mut container = self.create_container_state(&container_dir)?;
         container
