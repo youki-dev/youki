40a885137a5956dfbec680b8c02f81043fdf99a1
tommady <tommady@users.noreply.github.com>
2026-07-10 21:23:38 +0000

    handle issue 3626
    Signed-off-by: tommady <tommady@users.noreply.github.com>

--- crates/youki/src/commands/state.rs
+++ crates/youki/src/commands/state.rs
@@ -1,12 +1,14 @@
 use std::path::PathBuf;
 
 use anyhow::Result;
+use libcontainer::container::state::StateExporter;
 use liboci_cli::State;
 
 use crate::commands::load_container;
 
 pub fn state(args: State, root_path: PathBuf) -> Result<()> {
     let container = load_container(root_path, &args.container_id)?;
-    println!("{}", serde_json::to_string_pretty(&container.state)?);
+    let export = StateExporter::from(&container.state);
+    println!("{}", serde_json::to_string_pretty(&export)?);
     std::process::exit(0);
 }
