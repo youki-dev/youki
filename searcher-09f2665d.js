2da5f94affe59dd6ad4a3277c038011c26c33883
saku3 <saku3@users.noreply.github.com>
2026-07-16 07:07:42 +0000

    deploy: 6cb8db476a747df3afa8064e6134a89bfe31c142

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-77cda521.js',
+                path_to_root + 'searchindex-172a64dd.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
