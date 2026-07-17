a71f130028682be0c74aa5d69b7f343a42195382
tommady <tommady@users.noreply.github.com>
2026-07-17 04:46:54 +0000

    address comment: add created and owner into new state displaying
    Signed-off-by: tommady <tommady@users.noreply.github.com>

--- crates/youki/src/commands/state.rs
+++ crates/youki/src/commands/state.rs
@@ -2,6 +2,7 @@ use std::collections::HashMap;
 use std::path::{Path, PathBuf};
 
 use anyhow::Result;
+use chrono::{DateTime, Utc};
 use libcontainer::container::state::{ContainerStatus, State as ContainerState};
 use liboci_cli::State;
 use serde::Serialize;
@@ -26,6 +27,10 @@ pub struct StateExporter<'a> {
     pub bundle: &'a Path,
     #[serde(skip_serializing_if = "Option::is_none")]
     pub annotations: Option<&'a HashMap<String, String>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub created: Option<&'a DateTime<Utc>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub owner: Option<u32>,
 }
 
 impl<'a> From<&'a ContainerState> for StateExporter<'a> {
@@ -37,6 +42,8 @@ impl<'a> From<&'a ContainerState> for StateExporter<'a> {
             pid: state.pid,
             bundle: &state.bundle,
             annotations: state.annotations.as_ref(),
+            created: state.created.as_ref(),
+            owner: state.creator,
         }
     }
 }
